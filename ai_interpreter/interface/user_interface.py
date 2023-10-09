5ee195a76d516df43f2050a97242ff9daef63640
zeel sheladiya <46935793+zeelsheladiya@users.noreply.github.com>
2023-10-09 19:34:32 -0400

    added styling in terminal

--- ai_interpreter/interface/user_interface.py
+++ ai_interpreter/interface/user_interface.py
@@ -1,21 +1,33 @@
 from rich.console import Console
 from prompt_toolkit import PromptSession
 from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.styles import Style
 from ai_interpreter.cli import autocomplete
 from ai_interpreter.core.interpreter import generate_command, execute_command
 
+# Define custom styles
+custom_style = Style.from_dict({
+    'completion-menu.completion': 'bg:#008888 #ffffff',
+    'completion-menu.completion.current': 'bg:#00aaaa #000000',
+    'scrollbar.background': 'bg:#88aaaa',
+    'scrollbar.button': 'bg:#222222',
+})
+
 
 def main():
+
     console = Console()
+    completer = WordCompleter(autocomplete.commands, ignore_case=True)
 
     console.print("Welcome to the AI Interpreter Tool")
-
-    completer = WordCompleter(autocomplete.commands, ignore_case=True)
 
     session = PromptSession(
         completer=completer,
         complete_while_typing=True,
         reserve_space_for_menu=2,
+        auto_suggest=AutoSuggestFromHistory(), 
+        style=custom_style
     )
 
     while True:
